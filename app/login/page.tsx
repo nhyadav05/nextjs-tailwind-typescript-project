a3bfd0760251a042a48829286bc725aae12d8379
nhyadav05 <nehayadavneha0509@gmail.com>
2024-06-13 18:40:48 +0530

    working conditional layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,31 @@
+// // login.tsx
+// import RootLayout from "../layout";
+// import { LoginForm } from "./form";
+
+// const LoginPage = () => {
+//   return (
+//     <RootLayout isAuthPage isLoginPage> {/* Pass isAuthPage and isLoginPage props */}
+//       <div className="h-screen w-screen flex justify-center items-center bg-indigo-200">
+//         <LoginForm />
+//       </div>
+//     </RootLayout>
+//   );
+// };
+
+// export default LoginPage;
+
+
+// login.tsx
+import RootLayout from "../layout";
 import { LoginForm } from "./form";
 
 const LoginPage = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-indigo-200">
-     
-      <LoginForm />
-    
-    </div>
+    <RootLayout>
+      <div className="h-screen w-screen flex justify-center items-center bg-indigo-200">
+        <LoginForm />
+      </div>
+    </RootLayout>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+// page.tsx
+import RootLayout from "./layout";
 import { LoginForm } from "./login/form";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
-      <LoginForm />
-    </main>
+    <RootLayout>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
+        <LoginForm />
+      </main>
+    </RootLayout>
   );
 }
+export default Home;
