e7119eff46cb43afdd5cb4f0ac6986fba4d02b99
nhyadav05 <nehayadavneha0509@gmail.com>
2024-06-13 18:44:21 +0530

    working conditional layout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './home';
+import RootLayout from "../layout";
 
 const HomePage = () => {
   return (
+    <RootLayout >
     <div>
       <Home/>
     </div>
+        </RootLayout>
   );
 }
 
